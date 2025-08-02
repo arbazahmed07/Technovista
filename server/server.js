5c3bd8202cbe12a0f0010c9fb0c65d8ebf74323c
Sai Ritesh <sairiteshdomakuntla@gmail.com>
2025-08-02 06:51:47 +0530

    url change

--- server/server.js
+++ server/server.js
@@ -19,10 +19,11 @@ const io = socketIo(server, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ["http://localhost:3000", "http://localhost:5173"],
+//   credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
